5d334615c3929fca26718a369ccd4cb8639dc56c
esti08-hu <estioame@gmail.com>
2025-01-25 22:33:27 +0300

    feat: add global validation pipe with whitelisting and transformation

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,21 @@ import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import * as cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   // Enable CORS
   app.enableCors({
