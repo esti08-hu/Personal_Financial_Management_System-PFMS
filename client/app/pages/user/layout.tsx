f10bda8123d049035eee479d5f4dcbf8ab90b329
esti08-hu <estioame@gmail.com>
2025-08-17 23:35:54 +0300

    feat: enhance UI components across the application with improved styles, layouts, and responsiveness

--- client/app/pages/user/layout.tsx
+++ client/app/pages/user/layout.tsx
@@ -1,41 +1,32 @@
-"use client";
+"use client"
 
+import type React from "react"
 
-import Footer from "@/app/components/Footer";
-import UserNavbar from "@/app/components/user components/UserNavbar";
-import Head from "next/head";
-import { ToastContainer } from "react-toastify";
+import Footer from "@/app/components/Footer"
+import UserNavbar from "@/app/components/user components/UserNavbar"
+import { ToastContainer } from "react-toastify"
 
 interface LayoutProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <html lang="en">
-      <Head>
-        <title> My App</title>{" "}
-      </Head>
+    <div className="relative min-h-screen bg-gray-2 flex flex-col  from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 transition-colors duration-300">
+      <UserNavbar />
+      <div className="flex flex-col justify-center items-center h-40 font-black text-gray-800 dark:text-gray-100"></div>
 
-      <div className="relative min-h-screen bg-gray-2 flex flex-col bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 transition-colors duration-300">
-        <UserNavbar />
-        <div className="flex flex-col justify-center items-center h-40 font-black text-gray-800 dark:text-gray-100"></div>
+      <main className="flex-grow">
+        <div className="container mx-auto px-4 py-8">
+          <div className="mx-auto">{children}</div>
+        </div>
+      </main>
 
-        <main className="flex-grow">
-          <div className="container mx-auto px-4 py-">
-            <div className="mx-auto">{children}</div>
-          </div>
-        </main>
+      <Footer />
 
-        <Footer />
-      </div>
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-      />
-    </html>
-  );
-};
+      <ToastContainer position="top-center" autoClose={5000} hideProgressBar={false} />
+    </div>
+  )
+}
 
-export default Layout;
+export default Layout
